91dc5260f3128c66897aa515c41958f8b1319172
Triet Nguyen <triet.nguyen@etu.utc.fr>
2021-12-18 15:24:01 +0100

    add image

--- components/header.js
+++ components/header.js
@@ -2,33 +2,46 @@ import React from 'react';
 import { View, Image, StyleSheet, Dimensions, Button, TouchableOpacity } from 'react-native';
 import avatar from '../static/img/raiden_shogun.png';
 import logo from '../static/img/dream-real-logo-nav.png'
-import { Svg, Circle } from 'react-native-svg'
+import { Svg, Ellipse } from 'react-native-svg'
+import star from '../static/img/star.png'
 
 const screen = Dimensions.get("screen");
 const window = Dimensions.get("window")
 const figma_screen_w = 428;
 const figma_screen_h = 926;
 
-const APPBAR_HEIGHT = 118 * screen.height / figma_screen_h;
-
+const APPBAR_HEIGHT = 200 * screen.height / figma_screen_h;
 const Header = () => {
     return (
         <View style={styles.container}>
-            <Svg height={APPBAR_HEIGHT} width={screen.width} overflow="hidden">
-                <Circle
-                    cx={screen.width / 2}
-                    cy={`-${898 - APPBAR_HEIGHT + 2}`}
-                    r="898.5"
-                    fill="#3D3D4E"
-                    stroke="#3D3D4E"
-                    strokeWidth="2"
-                />
-                <View style={styles.content}>
+            <View style={styles.content}>
                     <Image source={avatar} style={styles.avatar}></Image> 
                     <Image source={logo} style={styles.logo}></Image>
                     <Button title="click" style={styles.button}></Button>
                     <Button title="check" style={styles.button}></Button>
-                </View>
+            </View>
+            <Svg height={APPBAR_HEIGHT} width={screen.width} overflow="hidden" style={styles.svg1}>
+                <Ellipse
+                    cx={screen.width / 2}
+                    cy={APPBAR_HEIGHT / 5}
+                    rx="180"
+                    ry="80"
+                    fill="#252A38"
+                    stroke="#252A38"
+                    strokeWidth="2"
+                />
+            </Svg>
+            <Svg height={APPBAR_HEIGHT * 2.5} width={screen.width} overflow="hidden" style={styles.svg2}>
+                <Ellipse
+                    cx={screen.width / 2}
+                    cy={APPBAR_HEIGHT * 2.5/ 3}
+                    rx="180"
+                    ry="120"
+                    fill="#3D3D4E"
+                    stroke="#3D3D4E"
+                    strokeWidth="2"
+                />
+                <Image source={star}></Image>
             </Svg>
         </View>
     )
@@ -37,7 +50,7 @@ const Header = () => {
 const styles = StyleSheet.create({
     container: {
         width: screen.width,
-        height: APPBAR_HEIGHT,
+        height: APPBAR_HEIGHT * 2.5,
         backgroundColor: "#252a38",
         overflow: "hidden"
     },
@@ -46,6 +59,8 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
+        position: 'relative',
+        zIndex: 1
     },
     avatar: {
         marginLeft: 10,
@@ -65,6 +80,15 @@ const styles = StyleSheet.create({
     button: {
         width: 15 * screen.width / figma_screen_w,
         height: 15 * screen.width / figma_screen_w,
+    },
+
+    svg1: {
+        position: "absolute",
+        zIndex: 0
+    },
+    svg2: {
+        position: "absolute",
+        zIndex: -1
     }
 })
 
